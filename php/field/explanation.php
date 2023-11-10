6916dc5bbd25b801cf3f295dee15c8710c4849b3
hoochicken <mareike.riegel@ql.de>
2023-11-10 23:47:10 +0100

    feat (font awesome) display added

---
+++ php/field/explanation.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package        mod_qlform
+ * @copyright    Copyright (C) 2023 ql.de All rights reserved.
+ * @author        Mareike Riegel mareike.riegel@ql.de
+ * @license        GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Joomla\CMS\Language\Text;
+
+defined('_JEXEC') or die;
+jimport('joomla.html.html');
+//import the necessary class definition for formfield
+jimport('joomla.form.formfield');
+
+class JFormFieldExplanation extends JFormField
+{
+    /**
+     * Method to retrieve the lists that resides in your application using the API.
+     *
+     * @return array The field option objects.
+     * @since 1.6
+     */
+    protected function getInput()
+    {
+        return sprintf('<div class="alert alert-info">%s</div>', Text::_('PLG_EDITORS-XTD_QLINSERTICON_EXPLANATION', ''));
+    }
+}
